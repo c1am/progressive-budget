3bde6fc9d46486de5875411a85db14dbf2cd4227
Cherry Lam <Cherry@cherrys-mbp.mskcc.org>
2021-08-27 16:32:44 -0400

    edited server js file

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.get('/', function(request, response) {
-  var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'));
-});
-
 app.use(logger("dev"));
 
 app.use(compression());
